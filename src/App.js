2527ed90269ca987cfc227245a10f5359e9b1d68
Artem Karpenko <c-artem.karpenko@hulu.com>
2022-01-16 08:29:37 +0200

    testing deployment

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi there!! - this is me!!!gg
+          Hi there!! - this is me!!!
         </p>
         <a
           className="App-link"
